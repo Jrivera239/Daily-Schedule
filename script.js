12cb9bd63786bde7a9ef5d23b0d0f876be2eba72
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-07-11 21:23:51 -0400

    adding timeblock and var

--- script.js
+++ script.js
@@ -4,3 +4,17 @@
     $("#currentDay").append(day);
 
 }
+
+//view the timeblocks for that day//
+var momentTime = moment().startOf("day").add(-1, "hour");
+var hour = moment().format("H");
+//working hours displayed //
+for (var i = 0; i < 24; i++) {
+    var HourSlot = momentTime.add(1, "hour").format("HH:mm A"); 
+    var MinuteSlot =   momentTime.add(0, "minute").format("mm"); 
+
+    //troubleshooting aid//
+    console.log(HourSlot)
+    console.log(MinuteSlot)
+
+    console.log (HourSlot % 12)
